764d8d2e1342afbe0d91e6c71f29df1743ad264c
Roker2 <dimaminko2000@gmail.com>
2021-01-27 23:39:11 +0300

    s2: Set USE_DEVICE_SPECIFIC_IPACFG_MGR

--- device.mk
+++ device.mk
@@ -221,6 +221,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # IPA Manager
+USE_DEVICE_SPECIFIC_IPACFG_MGR := true
 $(call inherit-product, $(LOCAL_PATH)/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
 
 # IRSC
